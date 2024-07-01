ce4b19a0aa93c997897146d911a679f406522078
manan0308 <63553691+manan0308@users.noreply.github.com>
2024-07-01 12:19:41 +0530

    Update Keyboard.js

--- client/src/components/Keyboard.js
+++ client/src/components/Keyboard.js
@@ -5,7 +5,7 @@ import { logError } from '../services/logService';
 const KEYBOARD_ROWS = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace'],
+  ['Backspace', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Enter'],
 ];
 
 const Keyboard = ({ usedLetters, onKeyPress, darkMode }) => {
